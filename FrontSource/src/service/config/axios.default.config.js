aad36dfcbec1338af649bc6bd2c8f2219677da13
chaser-one <2248487697@qq.com>
2025-06-22 15:54:02 +0800

    1

--- FrontSource/src/service/config/axios.default.config.js
+++ FrontSource/src/service/config/axios.default.config.js
@@ -1,5 +1,11 @@
 import axios from "axios"
-if (sessionStorage.getItem('Authorization')){
-  axios.defaults.headers.common['Authorization'] = sessionStorage.getItem('Authorization')
-}
+// if (sessionStorage.getItem('Authorization')){
+//   axios.defaults.headers.common['Authorization'] = sessionStorage.getItem('Authorization')
+// }
+axios.interceptors.request.use(config => {
+  if(sessionStorage.getItem('Authorization')){
+    config.headers.Authorization = sessionStorage.getItem('Authorization')
+  }
+  return config
+})
 export default axios
